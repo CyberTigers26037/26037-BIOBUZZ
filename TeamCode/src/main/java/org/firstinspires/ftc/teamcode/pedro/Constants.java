d5cb4241b5269fe58a9e8dc0fdec7a79ed6d3310
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-25 16:16:38 -0700

    Jermiah and Josh finished mecanum pedro pathing tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,25 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static Follower create(HardwareMap h) {
         // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
+
     }
+    public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
+        c.frontLeftName.set("front_left_drive");
+        c.frontRightName.set("front_right_drive");
+        c.backLeftName.set("back_left_drive");
+        c.backRightName.set("back_right_drive");
+        c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+        c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+        c.manualBrakeMode.set(true);
+    });
 }
